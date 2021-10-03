a1d170bed81b4994400a7f96d3ed293d36aed369
sweedfengpat <patza30596@gmail.com>
2021-10-03 12:10:01 +0700

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { MenuComponent } from './menu/menu.component';
+import { GreetingsComponent } from './greetings/greetings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    MenuComponent,
+    GreetingsComponent
   ],
   imports: [
     BrowserModule,
